2091bae7f39a5267be25d2dbf8f3707725202c6e
Jorge Santiago Alvarez Cuadros <sanjorgek@ciencias.unam.mx>
2017-05-29 13:16:33 -0500

    Modified to load more errors from config Modified style
    Resolves #5 #6

--- test/basic.test.js
+++ test/basic.test.js
@@ -1,8 +1,8 @@
-var Sails = require('sails').Sails;
-var request = require('supertest');
-var should = require('should');
+var Sails = require("sails").Sails;
+var request = require("supertest");
+var should = require("should");
 
- describe('Basic tests ::', function() {
+ describe("Basic tests ::", function() {
 
      // Var to hold a running sails app instance
      var sails;
@@ -17,7 +17,7 @@ var should = require('should');
          Sails().lift({
            hooks: {
              // Load the hook
-             "errorhandler": require('../'),
+             "errorhandler": require("../"),
              // Skip grunt (unless your hook uses it)
              "grunt": false
            },
@@ -25,7 +25,14 @@ var should = require('should');
            errorhandler: {
                 modelCodes: {
                     Cat: "11"
-                }
+                },
+
+                defaults: {
+                  gone: { message: "Service Gone", code: "410-00"},
+                  badRequest: {message: "my bad requeste message", code: "400-00"}
+                },
+
+                soloErrors: /^(badRequest|missingArguments|notAcceptable|notImplemented|serviceUnavailable|unsupportedMediaType|gone)$/
             }
          },function (err, _sails) {
            if (err) return done(err);
@@ -46,21 +53,21 @@ var should = require('should');
      });
 
      // Test that Sails can lift with the hook in place
-     it ('sails does not crash', function() {
+     it ("sails does not crash", function() {
          return true;
      });
 
-    describe('Validations on creation', function (){
-        it('creates an error with specific fields', function (done){
-            var errorCreated = sails.errorhandler.create('missingArguments','detailedInfo');
-            errorCreated.should.have.property('codeString','missingArguments');
-            errorCreated.should.have.property('detailedInfo','detailedInfo');
+    describe("Validations on creation", function (){
+        it("creates an error with specific fields", function (done){
+            var errorCreated = sails.errorhandler.create("missingArguments","detailedInfo");
+            errorCreated.should.have.property("codeString","missingArguments");
+            errorCreated.should.have.property("detailedInfo","detailedInfo");
             done();
         });
     });
 
-    describe('Validations on composition', function (){
-        it('should create default non-specific error', function (done) {
+    describe("Validations on build", function (){
+        it("should create default non-specific error", function (done) {
             var error = {
                 codeString: "badRequest",
                 detailedInfo: "detailedInfo"
@@ -69,11 +76,24 @@ var should = require('should');
             properError.should.have.property("error");
             properError.error.should.have.property("detailedInfo", "detailedInfo");
             properError.error.should.have.property("code", "400-00");
-            properError.error.should.have.property("message", "Bad Request");
+            properError.error.should.have.property("message", "my bad requeste message");
             done();
         });
 
-        it('should create default specific error', function (done) {
+        it("should create non-specific error from config", function (done) {
+            var error = {
+                codeString: "gone",
+                detailedInfo: "detailedInfo"
+            };
+            var properError = sails.errorhandler.buildError(error);
+            properError.should.have.property("error");
+            properError.error.should.have.property("detailedInfo", "detailedInfo");
+            properError.error.should.have.property("code", "410-00");
+            properError.error.should.have.property("message", "Service Gone");
+            done();
+        });
+
+        it("should create default specific error", function (done) {
             var error = {
                 codeString: "unauthorized",
                 icode: "00",
@@ -88,7 +108,7 @@ var should = require('should');
             done();
         });
 
-        it('should create model specific error', function (done) {
+        it("should create model specific error", function (done) {
             var error = {
                 codeString: "unauthorized",
                 icode: "13",
@@ -103,7 +123,21 @@ var should = require('should');
             done();
         });
 
-        it('should create serverError class', function (done) {
+        it("should create model specific error", function (done) {
+            var error = {
+                codeString: "unauthorized",
+                errorString: "Unauthorized model",
+                detailedInfo: "detailedInfo"
+            };
+            var properError = sails.errorhandler.buildError(error);
+            properError.should.have.property("error");
+            properError.error.should.have.property("detailedInfo", "detailedInfo");
+            properError.error.should.have.property("code", "401");
+            properError.error.should.have.property("message", "Unauthorized model");
+            done();
+        });
+
+        it("should create serverError class", function (done) {
             var error = {
                 codeString: "serverError",
                 icode: "01",
@@ -119,9 +153,9 @@ var should = require('should');
         });
     });
 
-    describe('Validations on composition', function (){
-        it('creates an error with default intern code', function (done){
-            var errorCreated = sails.errorhandler.create('unauthorized','detailedInfo');
+    describe("Validations on composition", function (){
+        it("creates an error with default intern code", function (done){
+            var errorCreated = sails.errorhandler.create("unauthorized","detailedInfo");
             var toResponse = sails.errorhandler.compose(errorCreated);
             toResponse.should.have.property("icode", "00");
             toResponse.should.have.property("detailedInfo", "detailedInfo");
@@ -130,8 +164,8 @@ var should = require('should');
             done();
         });
 
-        it('creates an error with model intern code', function (done){
-            var errorCreated = sails.errorhandler.create('unauthorizedCat','detailedInfo');
+        it("creates an error with model intern code", function (done){
+            var errorCreated = sails.errorhandler.create("unauthorizedCat","detailedInfo");
             var toResponse = sails.errorhandler.compose(errorCreated);
             toResponse.should.have.property("icode", "11");
             toResponse.should.have.property("detailedInfo", "detailedInfo");
@@ -140,42 +174,53 @@ var should = require('should');
             done();
         });
 
-        it('creates an error with default response codeString', function (done){
-            var errorCreated = sails.errorhandler.create('notAcceptable','detailedInfo');
+        it("creates an error without model intern code", function (done){
+            var errorCreated = sails.errorhandler.create("unauthorizedDog","detailedInfo");
+            var toResponse = sails.errorhandler.compose(errorCreated);
+            toResponse.should.have.property("icode");
+            should.not.exist(toResponse.icode);
+            toResponse.should.have.property("detailedInfo", "detailedInfo");
+            toResponse.should.have.property("errorString", "Unuathorized Dog");
+            toResponse.should.have.property("codeString", "unauthorized");
+            done();
+        });
+
+        it("creates an error with default response codeString", function (done){
+            var errorCreated = sails.errorhandler.create("notAcceptable","detailedInfo");
             var toResponse = sails.errorhandler.compose(errorCreated);
             toResponse.should.have.property("codeString", "notAcceptable");
-            toResponse.should.have.property('detailedInfo','detailedInfo');
+            toResponse.should.have.property("detailedInfo","detailedInfo");
             should.not.exist(toResponse.code);
             should.not.exist(toResponse.errorString);
             done();
         });
 
-        it('creates a serverError type from other error', function (done){
-            var errorCreated = sails.errorhandler.create('dataBaseError','detailedInfo');
+        it("creates a serverError type from other error", function (done){
+            var errorCreated = sails.errorhandler.create("dataBaseError","detailedInfo");
             var toResponse = sails.errorhandler.compose(errorCreated);
-            toResponse.should.have.property('codeString','serverError');
-            toResponse.should.have.property('detailedInfo','detailedInfo');
-            toResponse.should.have.property('icode', '01');
+            toResponse.should.have.property("codeString","serverError");
+            toResponse.should.have.property("detailedInfo","detailedInfo");
+            toResponse.should.have.property("icode", "01");
             should.not.exist(toResponse.errorString);
             done();
         });
 
-        it('creates a serverError type', function (done){
-            var errorCreated = sails.errorhandler.create('serverError','detailedInfo');
+        it("creates a serverError type", function (done){
+            var errorCreated = sails.errorhandler.create("serverError","detailedInfo");
             var toResponse = sails.errorhandler.compose(errorCreated);
-            toResponse.should.have.property('codeString','serverError');
-            toResponse.should.have.property('detailedInfo','detailedInfo');
-            toResponse.should.have.property('icode', '00');
+            toResponse.should.have.property("codeString","serverError");
+            toResponse.should.have.property("detailedInfo","detailedInfo");
+            toResponse.should.have.property("icode", "00");
             should.not.exist(toResponse.errorString);
             done();
         });
 
-        it('creates a serverError type from non-existent response', function (done){
-            var errorCreated = sails.errorhandler.create('notExistent','detailedInfo');
+        it("creates a serverError type from non-existent response", function (done){
+            var errorCreated = sails.errorhandler.create("notExistent","detailedInfo");
             var toResponse = sails.errorhandler.compose(errorCreated);
-            toResponse.should.have.property('codeString','serverError');
-            toResponse.should.have.property('detailedInfo','detailedInfo');
-            toResponse.should.have.property('icode', '00');
+            toResponse.should.have.property("codeString","serverError");
+            toResponse.should.have.property("detailedInfo","detailedInfo");
+            toResponse.should.have.property("icode", "00");
             should.not.exist(toResponse.errorString);
             done();
         });
